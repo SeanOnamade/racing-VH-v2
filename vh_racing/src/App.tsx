c1c4597d162e12ea234ed0cdc90c29388dc4f205
SeanOnamade <100744222+SeanOnamade@users.noreply.github.com>
2024-09-28 19:24:40 -0500

    git ignore again? update vhracing and add env files but you won't see that lol

--- vh_racing/src/App.tsx
+++ vh_racing/src/App.tsx
@@ -2,43 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
-<<<<<<< HEAD
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import RacingLines from './pages/racingLineCreation/racingLine';
 import Signup from './pages/Signup';
-import Login from './pages/Signup';
+import Login from './pages/Login';
+import { Sign } from 'crypto';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-const App: React.FC = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-=======
-import RacingLines from '../pages/racingLineCreation/racingLine';
 
 // Main App component
-function App() {
+const App:React.FC = () => {
   return (
     <Router>
       <Routes>
@@ -65,7 +36,8 @@ function App() {
 
         {/* Ethan page route - Using RacingLines component */}
         <Route path="/ethan" element={<RacingLines />} />
->>>>>>> 39daf29cd1ecaad1be61e8f05b14a9f74b63146a
+        <Route path="/signup" element={<Signup />} /> 
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
--- vh_racing/src/pages/racingLineCreation/racingLine.tsx
+++ vh_racing/src/pages/racingLineCreation/racingLine.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 // EthanPage component
 const EthanPage: React.FC = () => {
@@ -12,25 +13,40 @@ const EthanPage: React.FC = () => {
 };
 
 // Main RacingLines component
+// const RacingLines: React.FC = () => {
+//   return (
+//     <Router>
+//       <Routes>
+//         {/* Default Route - Home Page */}
+//         <Route
+//           path="/"
+//           element={
+//             <div>
+//               <h1>Racing Lines Home</h1>
+//               <p>Edit <code>src/racingLines.tsx</code> to modify this page.</p>
+//             </div>
+//           }
+//         />
+
+//         {/* Route for /ethan */}
+//         <Route path="/ethan" element={<EthanPage />} />
+//       </Routes>
+//     </Router>
+//   );
+// };
+
 const RacingLines: React.FC = () => {
   return (
-    <Router>
-      <Routes>
-        {/* Default Route - Home Page */}
-        <Route
-          path="/"
-          element={
-            <div>
-              <h1>Racing Lines Home</h1>
-              <p>Edit <code>src/racingLines.tsx</code> to modify this page.</p>
-            </div>
-          }
-        />
+    <div>
+      <h1>Racing Lines Home</h1>
+      <p>Edit <code>src/racingLines.tsx</code> to modify this page.</p>
 
-        {/* Route for /ethan */}
+      {/* Nested Routes - Place the Route inside Routes */}
+      <Routes>
+        {/* Define the route here */}
         <Route path="/ethan" element={<EthanPage />} />
       </Routes>
-    </Router>
+    </div>
   );
 };
 
